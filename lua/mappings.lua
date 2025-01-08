5bd3fcb12c5a4a7d389d3ddc8ce706815459edbb
dongg <donggeuri0320@gmail.com>
2025-01-08 21:52:07 +0900

    add mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -42,9 +42,27 @@ vim.keymap.set("n", "<Space>", "<Nop>", { desc = "Unmap space for leader key" })
 vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete without yanking" })
 vim.keymap.set("n", "<leader>p", "\"_dP", { desc = "Paste without overwriting register" })
 
-vim.keymap.set("n", ";d", "viwd", { desc = "Delete word" })
-vim.keymap.set("n", ";c", "yiw", { desc = "Copy word" })
-vim.keymap.set("n", ";v", "yiwpyiw", { desc = "Paste word" })
+-- ;c: 현재 단어를 변수에 저장
+local current_word = ""
+
+vim.keymap.set("n", ";c", function()
+  current_word = vim.fn.expand("<cword>")
+end, { desc = "Copy current word to a variable" })
+
+-- ;v: 현재 단어를 변수에 저장된 단어로 교체
+vim.keymap.set("n", ";v", function()
+  local search_word = vim.fn.expand("<cword>")
+  vim.fn.setreg("/", search_word) -- 검색 패턴 설정
+  vim.cmd('normal! cgn') -- 현재 단어 검색 후 교체
+  vim.api.nvim_put({ current_word }, "", true, true) -- 변수 내용을 삽입
+end, { desc = "Replace current word with saved word" })
+
+-- ;d: 현재 단어 삭제
+vim.keymap.set("n", ";d", function()
+  local search_word = vim.fn.expand("<cword>")
+  vim.fn.setreg("/", search_word) -- 검색 패턴 설정
+  vim.cmd('normal! dgn') -- 현재 단어 삭제
+end, { desc = "Delete current word" })
 
 -- Visual 모드
 vim.keymap.set("x", "<leader>d", "\"_d", { desc = "Delete without yanking (visual)" })
