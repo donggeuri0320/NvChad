3768864b1e5ce9e1249117f856275777f607ac30
donggeuri0320 <donggeuri0320@gmail.com>
2025-07-08 23:48:50 +0900

    add flash.lua

--- lua/plugins/hop.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "phaazon/hop.nvim", -- 공식 저장소 이름은 'phaazon/hop.nvim' 입니다.
-    branch = "v2",      -- hop.nvim v2 버전을 사용하는 것을 권장합니다.
-    opts = {},          -- 여기에 hop.nvim의 설정을 직접 넣을 수 있습니다.
-    lazy = false,       -- NvChad 시작 시 바로 로드되도록 설정 (탐색 플러그인이므로 보통 이렇게 함)
-  },
-}
